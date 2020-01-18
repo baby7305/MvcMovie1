eb4c7ed386ed0185cf49980cb938f79d9e76cb6d
baby7305 <songhaibei7305@163.com>
2020-01-18 17:21:25 +0800

    添加数据库上下文类
    还补充了一个nuget包

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MvcMovie1.Models;
+
+namespace MvcMovie1.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
